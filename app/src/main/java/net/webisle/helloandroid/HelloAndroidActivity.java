c85bb3a65ce4be29fd487824099783a1cef007d6
Tomonori Ueno <t.uepon@gmail.com>
2018-10-09 23:01:08 +0900

    first commit

---
+++ app/src/main/java/net/webisle/helloandroid/HelloAndroidActivity.java
@@ -0,0 +1,13 @@
+package net.webisle.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloAndroidActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_android);
+    }
+}
